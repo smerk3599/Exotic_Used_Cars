86d465b9a48c776e9fd1cac367b7bdad74f72f01
Brandon Mazikowski <brandon.mazikowski@gmail.com>
2021-01-13 14:17:06 -0800

    finished top half of controller

---
+++ controller/cars_controller.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const cars = express.Router()
+
+const Car = require('../models/cars.js')
+
+cars.get('/', (req, res) => {
+  Car.find({}, (error, foundCars) => {
+    res.json(foundCars)
+  })
+})
+cars.post('/', (req, res) => {
+  Car.create(req.body, (error, createCar) => {
+    Car.find({}, (error, foundAnimals) => {
+      res.json(foundCars)
+    })
+  })
+})
+cars.put('/:id', (req, res) => {
+  Car.findByIdAndUpdate(
+    req.params.id,
+    req.body,
+    {new: true},
+    (error, updateCar) => {
+      if(error){
+        res.send(error)
+      } else {
+        Car.find({}, (error, foundCars) => {
+          res.json(foundCars)
+        })
+      }
+    }
+  )
+})
