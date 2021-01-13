25e7df4b6b971af65d642254db91794ae6165cd1
Brandon Mazikowski <brandon.mazikowski@gmail.com>
2021-01-13 14:01:17 -0800

    finished models cars.js

---
+++ models/cars.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose')
+
+const carSchema = new mongoose.Schema(
+  {
+    name: {type: String, required: true},
+    year: {type: Number, required: true},
+    make: {type: String, required: true},
+    model: {type: String, required: true},
+    price: {type: Number, required: true},
+    image: {type: String, required: true},
+  }
+)
+
+const Car = mongoose.model('Car', carSchema)
+
+module.exports = Car
