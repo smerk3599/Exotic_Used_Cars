d766cf36e09d4fff60219e4791af1cf77266e06b
Steven Mercer <stevenmercer9913@gmail.com>
2021-01-13 16:19:07 -0800

    event handlers in React are complete

---
+++ public/js/app.js
@@ -0,0 +1,66 @@
+class App extends React.Component {
+  state = {
+    year: '',
+    make: '',
+    model: '',
+    price: '',
+    image: '',
+    cars:[]
+  }
+
+handleChange = (event) => {
+  this.setState({
+    [event.target.id]: event.target.value
+  })
+}
+
+handleSubmit = (event) => {
+  event.preventDefault();
+  axios
+    .post('/cars', this.state)
+    .then((response) =>
+      this.setState({
+        cars: response.data,
+        year: 1913,
+        make: '',
+        model: '',
+        price: 0,
+        image: ''
+    })
+  )
+}
+
+deleteCar = (event) => {
+  axios
+  .delete('/cars/' + event.target.value)
+  .then((response) => {
+    this.setState({
+      cars: response.data
+    })
+  })
+}
+
+updateCar = (event) => {
+  event.preventDefault();
+  const id = event.target.id;
+  axios
+    .put('/cars/' + id, this.state)
+    .then((response) => {
+      this.setState({
+        cars: response.data,
+        year: 1913,
+        make: '',
+        model: '',
+        price: 0,
+        image: ''
+      })
+    })
+}
+
+componentDidMount = () => {
+  axios.get('/cars').then(response => {
+    this.setState({
+      birds: response.data
+    })
+  })
+}
